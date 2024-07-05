822ce3265f9d4b094d47241fed941a09d2347f6e
Tsutomu YANO <tsutomuyano@shelfinc.com>
2024-07-05 12:34:55 +0900

    changed the project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pedestal-extensions "1.0-SNAPSHOT"
+(defproject org.clojars.t_yano/pedestal-extensions "1.0-SNAPSHOT"
   :description "Utility functions for Pedestal"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
