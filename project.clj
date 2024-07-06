e7751f32552d8666e1760b5757bafd191b23a55f
Tsutomu YANO <tsutomu.yano@uzabase.com>
2024-07-06 11:16:16 +0900

    Changed the version to 1.0.0

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject org.clojars.t_yano/pedestal-extensions "1.0-SNAPSHOT"
+(defproject org.clojars.t_yano/pedestal-extensions "1.0.0"
   :description "Utility functions for Pedestal"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/tyano/pedestal-extensions"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :profiles
   {:provided {:dependencies [[io.pedestal/pedestal.service "0.6.4"]]}}
-  :repl-options {:init-ns pedestal-extensions.core})
+  :repl-options {:init-ns org.clojars.t-yano.pedestal-extensions.handler})
